1389c0d6bba19c01d0a5f912d0979939343a1944
Sergii Liebodkin <Sergii.Liebodkin@halliburton.com>
2019-07-25 11:28:32 +0300

    base markup

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,3 +1,13 @@
+import * as Plotly from "plotly.js"
+
 window.onload = event => {
-    console.log("Hello!")
+    let root = document.getElementById('root');
+    Plotly.plot(root,
+        [{
+            x: [1, 2, 3, 4, 5],
+            y: [1, 2, 4, 8, 16]
+        }],
+        {
+            margin: { t: 0 }
+        });
 }
