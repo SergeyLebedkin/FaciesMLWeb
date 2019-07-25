cd6a24a465903559fc344598c9d77ae9862dd8b9
Sergii Liebodkin <Sergii.Liebodkin@halliburton.com>
2019-07-25 14:17:05 +0300

    folder rename

--- src/js/index.ts
+++ src/js/index.ts
@@ -2,12 +2,5 @@ import * as Plotly from "plotly.js"
 
 window.onload = event => {
     let root = document.getElementById('root');
-    Plotly.plot(root,
-        [{
-            x: [1, 2, 3, 4, 5],
-            y: [1, 2, 4, 8, 16]
-        }],
-        {
-            margin: { t: 0 }
-        });
+    Plotly.plot(root, [{ x: [1, 2, 3, 4, 5], y: [1, 2, 4, 8, 16] }], { margin: { t: 0 } });
 }
